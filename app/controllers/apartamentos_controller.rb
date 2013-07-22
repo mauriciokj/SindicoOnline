181c232328b4b36b5f1c9f7ca92745a0c902e41c
Mauricio Krzesinski Junior <mauriciokj@gmail.com>
2013-07-22 18:28:45 -0300

    teste

--- app/controllers/apartamentos_controller.rb
+++ app/controllers/apartamentos_controller.rb
@@ -1,5 +1,8 @@
 class ApartamentosController < ApplicationController
 	before_filter :authenticate_usuario!
 	active_scaffold :apartamento do |conf|
+		conf.columns = [:bloco, :numero, :imovel]
+		conf.columns[:imovel].form_ui = :select
+		conf.columns[:imovel].clear_link
 	end
 end
