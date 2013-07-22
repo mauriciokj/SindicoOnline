c70295c688adfe9fa3de27a8e011c5ab0f59a6ed
Mauricio Krzesinski Junior <mauriciokj@gmail.com>
2013-07-22 18:16:03 -0300

    aa

--- config/application.rb
+++ config/application.rb
@@ -55,8 +55,6 @@ module SindicoOnline
 
     # Enable the asset pipeline
     config.assets.enabled = true
-    config.assets.initialize_on_precompile = false
-
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
   end
