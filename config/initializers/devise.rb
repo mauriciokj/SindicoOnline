28da31af14cd00d87be0b6fc8fb2cb74085472a9
Mauricio Junior <mauriciokj@gmail.com>
2014-02-24 22:16:44 -0300

    gem.lock

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -5,6 +5,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class with default "from" parameter.
   config.mailer_sender = "please-change-me-at-config-initializers-devise@example.com"
+  config.secret_key = 'b26944d10534b9e4e38625b17b1ea831a46b1a1ffcb5acc1564c3f383843a6ecf54f438aab4638ac784d30a4fe285b970b8c7f91834abf965ce222c35a5e24fa'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = "Devise::Mailer"
